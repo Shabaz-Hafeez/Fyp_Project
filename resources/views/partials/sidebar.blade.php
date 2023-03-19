fb6bddae1d664911bf16af6394f09e931b3e2fdf
{shahbaz-hafeez} <{shahbazhafeez888@gmail.com}>
2023-03-19 22:22:17 +0500

    Sidebar Setting

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -16,50 +16,49 @@
                  <li><a href="#"><i class="uil uil-dashboard me-2 d-inline-block"></i>Dashboard</a></li>
 
                  
-                 <li><a href="appointment.html"><i class="uil uil-stethoscope me-2 d-inline-block"></i>Appointment</a>
+                 <li><a href="appointment.html"><i class="uil uil-stethoscope me-2 d-inline-block"></i>Services</a>
                  </li>
 
                  <li class="sidebar-dropdown">
                      <a href="javascript:void(0)"><i class="uil-shopping-cart-alt me-2 d-inline-block"></i>Product Management</a>
                      <div class="sidebar-submenu">
                          <ul>
-                             <li><a href="">Add Products</a></li>
-                             <li><a href="add-doctor.html">Products</a></li>
-                             <li><a href="dr-profile.html">Profile</a></li>
+                             <li><a href="">Manage Products</a></li>
+                             <li><a href="add-doctor.html">Product Categories</a></li>
+                             
                          </ul>
                      </div>
                  </li>
 
                  <li class="sidebar-dropdown">
-                     <a href="javascript:void(0)"><i class="uil uil-wheelchair me-2 d-inline-block"></i>Patients</a>
+                     <a href="javascript:void(0)"><i class="uil uil-wheelchair me-2 d-inline-block"></i>Order Management</a>
                      <div class="sidebar-submenu">
                          <ul>
-                             <li><a href="patients.html">All Patients</a></li>
-                             <li><a href="add-patient.html">Add Patients</a></li>
-                             <li><a href="patient-profile.html">Profile</a></li>
+                             <li><a href="patients.html">All Orders</a></li>
+                             <li><a href="add-patient.html">Add Orders</a></li>
                          </ul>
                      </div>
                  </li>
 
                  <li class="sidebar-dropdown">
-                     <a href="javascript:void(0)"><i class="uil uil-apps me-2 d-inline-block"></i>Apps</a>
+                     <a href="javascript:void(0)"><i class="uil uil-apps me-2 d-inline-block"></i>Inventory Management</a>
                      <div class="sidebar-submenu">
                          <ul>
-                             <li><a href="chat.html">Chat</a></li>
-                             <li><a href="email.html">Email</a></li>
-                             <li><a href="calendar.html">Calendar</a></li>
+                             <li><a href="chat.html">Stock Details</a></li>
+                             <li><a href="email.html">Stock Threshold</a></li>
+                             {{-- <li><a href="calendar.html">Calendar</a></li> --}}
                          </ul>
                      </div>
                  </li>
 
                  <li class="sidebar-dropdown">
-                     <a href="javascript:void(0)"><i class="uil uil-shopping-cart me-2 d-inline-block"></i>Pharmacy</a>
+                     <a href="javascript:void(0)"><i class="uil uil-shopping-cart me-2 d-inline-block"></i>Customer Management</a>
                      <div class="sidebar-submenu">
                          <ul>
-                             <li><a href="shop.html">Shop</a></li>
-                             <li><a href="product-detail.html">Shop Detail</a></li>
-                             <li><a href="shopcart.html">Shopcart</a></li>
-                             <li><a href="checkout.html">Checkout</a></li>
+                             <li><a href="shop.html">Information</a></li>
+                             <li><a href="product-detail.html">Order History</a></li>
+                             <li><a href="shopcart.html">Payment Detail</a></li>
+                             {{-- <li><a href="checkout.html">Checkout</a></li> --}}
                          </ul>
                      </div>
                  </li>
@@ -67,16 +66,16 @@
                 
 
                  <li class="sidebar-dropdown">
-                     <a href="javascript:void(0)"><i class="uil uil-file me-2 d-inline-block"></i>Pages</a>
+                     <a href="javascript:void(0)"><i class="uil uil-file me-2 d-inline-block"></i>Report and Analytics</a>
                      <div class="sidebar-submenu">
                          <ul>
-                             <li><a href="faqs.html">FAQs</a></li>
-                             <li><a href="review.html">Reviews</a></li>
-                             <li><a href="invoice-list.html">Invoice List</a></li>
-                             <li><a href="invoice.html">Invoice</a></li>
+                             <li><a href="faqs.html">Sales Report</a></li>
+                             <li><a href="review.html">Inventory Report</a></li>
+                             <li><a href="invoice-list.html">Customer Data</a></li>
+                             {{-- <li><a href="invoice.html">Invoice</a></li>
                              <li><a href="terms.html">Terms & Policy</a></li>
                              <li><a href="privacy.html">Privacy Policy</a></li>
-                             <li><a href="blank-page.html">Blank Page</a></li>
+                             <li><a href="blank-page.html">Blank Page</a></li> --}}
                          </ul>
                      </div>
                  </li>
@@ -85,11 +84,13 @@
 
                  <li class="sidebar-dropdown">
                      <a href="javascript:void(0)"><i
-                             class="uil uil-sign-in-alt me-2 d-inline-block"></i>Authentication</a>
+                             class="uil uil-sign-in-alt me-2 d-inline-block"></i>Payment Gateway Management</a>
                      <div class="sidebar-submenu">
                          <ul>
-                             <li><a href="{{ route('login')}}">Login</a></li>
-                             <li><a href="{{route('register')}}">Signup</a></li>
+                             <li><a href="{{ route('login')}}">Payment Options</a></li>
+                             <li><a href="{{route('register')}}">Payment Settings</a></li>
+                            <li><a href="{{route('register')}}">Payment History</a></li>
+
                             
                          </ul>
                      </div>
